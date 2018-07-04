f80d1a10b812f3dfc0cd3a3a0c254c831cd9825d
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-04 16:40:49 +0200

    Add method

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ function Phone(brand, model, color, price) {
     this.color = color;
     this.price = price;
 }
+
+Phone.prototype.printInfo = function () {
+    console.log('The phone brand is ' + this.brand + ', model: ' + this.model + ', color: ' + this.color + ' and the price is: ' +this.price + '.');
+}
