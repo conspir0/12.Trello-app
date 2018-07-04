086586e1f04f28d1e6521a7d1d6d38f19db54262
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-04 19:28:58 +0200

    Create context

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function Button(text){
+    this.text = text || 'Write Button value';
+}
+
+Button.prototype = {
+    create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function(){
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+    }
+}
+
+var btn1 = new Button('Click me!');
+btn1.create();
