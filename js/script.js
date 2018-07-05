7b9e5d3083707afe050254be9ab4b02e8d567e83
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-05 15:05:55 +0200

    Add Class Column in JS, Column Template in HTML

--- js/script.js
+++ js/script.js
@@ -25,4 +25,24 @@ document.addEventListener('DOMContentLoaded', function(){
         
         return element;
     }
+    
+    //Class Column
+    function Column(name) {
+        var self = this;
+        
+        this.id = randomString();
+        this.name = name;
+        this.element = generateTemplate('column-template', {
+            name: this.name
+        });
+        this.element.querySelector('.column').addEventListener('click', function(event) {
+            if (event.target.classList.contains('btn-delete')) {
+                self.removeColumn();
+            }
+            if (event.target.classList.contains('add-card')) {
+                self.addCard(new Card(prompt('Enter the name of the card')));
+            }
+        });
+    }
+    
 });
