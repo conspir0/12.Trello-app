75a458f1fc00a6c66524cd90e7064c2f24f01603
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-05 14:44:38 +0200

    Add generate ID, template in JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Code will be working after loaded DOM
+document.addEventListener('DOMContentLoaded', function(){
+    
+    // Generate random ID
+    function randomString() {
+        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+        var str = '';
+        
+        for (var i = 0; i < 10; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        
+        return str;
+    }
+    
+    // Generate tamplate
+    function generateTemplate(name, data, basicElement) {
+        var template = document.getElementById(name).innerHTML;
+        var element = document.createElement(basicElement) || 'div';
+        
+        Mustache.parse(template);
+        element.innerHTML = Mustache.render(template, data);
+        
+        return element;
+    }
+});
